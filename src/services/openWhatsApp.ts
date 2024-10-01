0371cb846532b2564b1a878ae2fe33c579a54092
BrunoLandim <brunolandim13@hotmail.com>
2024-10-01 14:01:21 -0300

    change number

--- src/services/openWhatsApp.ts
+++ src/services/openWhatsApp.ts
@@ -1,5 +1,5 @@
 export function openWhatsApp() {
-  const phoneNumber = '556199670870';
+  const phoneNumber = '556186065556';
   const message = `Olá! Estou visitando o site e gostaria de saber mais sobre os serviços de captação de imagens aéreas com drones.\nPoderia me passar mais informações, por favor?`;
   const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
   window.open(url, '_blank');
